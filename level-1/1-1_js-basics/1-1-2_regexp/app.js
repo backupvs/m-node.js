75a5b6e60f995c559f68bb459f9f391b4533fe18
backupvs <dmitrygolubev@gmail.com>
2023-01-03 02:43:38 +0200

    Added validatePhone and fixed emailPattern

--- level-1/1-1_js-basics/1-1-2_regexp/app.js
+++ level-1/1-1_js-basics/1-1-2_regexp/app.js
@@ -1,3 +1,15 @@
+const Validator = {
+    validateEmail(string) {
+        const emailPattern = /(^[^.+-][a-zA-Z0-9.+-]{1,20})@([\w.!$%&'*+/=?^-]{1,15})\.([a-z]{1,5})$/g
+        return emailPattern.test(string);
+    },
+
+    validatePhone(string) {
+        const phonePattern = /^([\s-]*\+[\s-]*3[\s-]*8[\s-]*)?[\s-]*\(?([\s-]*0[\s-]*\d[\s-]*\d[\s-]*)\)?([\s-]*\d{1}[\s-]*\d{1}[\s-]*\d{1}[\s-]*\d{1}[\s-]*\d{1}[\s-]*\d{1}[\s-]*\d{1}[\s-]*)$/g;
+        return phonePattern.test(string);
+    }
+}
+
 const emails = [
     /* Valid */
     "firstpart@secondpart.end",
@@ -13,13 +25,24 @@ const emails = [
     "firstpart@.se.enddeded"
 ];
 
-const Validator = {
-    validateEmail(string) {
-        const emailPattern = /\b(^[^.+-][a-zA-Z0-9.+-]{1,20})@([\w.!$%&'*+/=?^-]{1,15})\.([a-z]{1,5})\b/g
-        return emailPattern.test(string);
-    }
-}
+const phones = [
+    /* Valid */
+    "+38 (099) 567 8901",
+    "+38 099 5 6 7 8 9  01",
+    "(09-9) 567-890-1",
+    "--  (099) 567 890-1",
+
+    /* Invalid */
+    "+38 (099) 567 8901 0",
+    "+38 099 a0000000",
+    "38 (0989) 567 8901",
+    "+48 (0989) 567 8901"
+];
 
 for (let email of emails) {
-    console.log(email + ": " + Validator.validateEmail(email));
+    console.log((Validator.validateEmail(email) ? "[+] " : "[-] ") + email);
+}
+
+for (let phone of phones) {
+    console.log((Validator.validatePhone(phone) ? "[+] " : "[-] ") + phone);
 }
