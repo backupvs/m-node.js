87b5c164741c8676af7534054f74c74e4d9dd03d
backupvs <dmitryholubev@gmail.com>
2023-02-18 21:12:15 +0200

    Little codestyle refactoring

--- level-3/3-2_library-mvc/src/controllers/root.controller.ts
+++ level-3/3-2_library-mvc/src/controllers/root.controller.ts
@@ -4,23 +4,19 @@ import Book from "@models/Book.model";
 export const findBooks = async (req: Request, res: Response) => {
     const offset = +req.query.offset!;
     const limit = +req.query.limit!;
-    const numberOfBooks = req.query.numberOfBooks!;
+    const totalBooks = req.query.totalBooks!;
 
-    const search = makeSearchQuery(req.query.search?.toString() || "");
-    const author = makeSearchQuery(req.query.author?.toString() || "");
-    const year = makeSearchQuery(req.query.year?.toString() || "");
+    const books = await Book.find(
+        createSearchQuery(req.query.search?.toString()),
+        createSearchQuery(req.query.author?.toString()),
+        createSearchQuery(req.query.year?.toString()),
+        offset.toString(),
+        limit.toString()
+    );
 
-    const books = await Book.find(search, author, year, offset.toString(), limit.toString());
-    
-    res.render("books", {
-        books,
-        numberOfBooks,
-        offset,
-        limit,
-        searchQuery: req.query.search
-    });
+    res.render("books", { books, totalBooks, offset, limit, searchQuery: req.query.search });
 }
 
-const makeSearchQuery = (query: string) => "%" + query + "%";
+const createSearchQuery = (query: string | undefined): string => `%${query || ""}%`;
 
 export default { findBooks };
--- level-3/3-2_library-mvc/src/middlewares/paginationValidator.ts
+++ level-3/3-2_library-mvc/src/middlewares/paginationValidator.ts
@@ -1,20 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import Book from "@models/Book.model";
 
-export const paginationValidator = async (req: Request, res: Response, next: NextFunction) => {
-    const numberOfBooks = await Book.getNumberOfAll();
-    let limit = req.query.limit ? +req.query.limit || +process.env.BOOKS_LIMIT! : +process.env.BOOKS_LIMIT!;
-    let offset = req.query.offset ? +req.query.offset || 0 : 0;
+const DEFAULT_BOOKS_LIMIT = 12;
+const DEFAULT_BOOKS_OFFSET = 0;
 
-    if (limit <= 0) limit = +process.env.BOOKS_LIMIT!;
-    if (offset >= numberOfBooks) offset = numberOfBooks - limit;
+export const paginationValidator = async (req: Request, res: Response, next: NextFunction) => {
+    const totalBooks = await Book.getNumberOfAll();
+
+    let limit = Number(req.query.limit) || DEFAULT_BOOKS_LIMIT;
+    let offset = Number(req.query.offset) || DEFAULT_BOOKS_OFFSET;
+
+    if (limit <= 0) limit = DEFAULT_BOOKS_LIMIT;
+    if (offset >= totalBooks) offset = totalBooks - limit;
     if (offset < 0) offset = 0;
 
     req.query.limit = limit.toString();
     req.query.offset = offset.toString();
-    req.query.numberOfBooks = numberOfBooks;
+    req.query.totalBooks = totalBooks.toString();
 
     next();
 }
 
-export default paginationValidator
+export default paginationValidator;
